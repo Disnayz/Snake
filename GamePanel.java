8197e75badb669ea725bb39997910abab7baa067
Disnayz <159566429+Disnayz@users.noreply.github.com>
2024-05-13 23:51:40 +0200

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 700;
@@ -11,17 +12,42 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int OBJECT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / OBJECT_SIZE;
     static final int DELAY = 75 ;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 5;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
     GamePanel(){
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame(){
+    newApple();
+    running = true;
+    timer = new Timer(DELAY,this);
+    timer.start();
+    }
+    public void newApple(){
 
     }
     public void paintComponent(Graphics g){
-
+    super.paintComponent(g);
+    draw(g);
     }
     public void draw(Graphics g){
-
+    for(int i = 0;i < SCREEN_HEIGHT/OBJECT_SIZE;i++){
+        g.drawLine(i * OBJECT_SIZE,0,i * OBJECT_SIZE,SCREEN_HEIGHT);
+        g.drawLine(0,i * OBJECT_SIZE, SCREEN_WIDTH,i * OBJECT_SIZE);
+    }
     }
     public void move(){
 
